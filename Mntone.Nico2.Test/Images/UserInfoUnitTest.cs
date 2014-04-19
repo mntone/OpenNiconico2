8abb85a05ff5b6ff0073c3b644abf9f417df8123
mntone <sd8@live.jp>
2014-04-19 12:10:54 +0900

    Update test method.

--- Mntone.Nico2.Test/Images/UserInfoUnitTest.cs
+++ Mntone.Nico2.Test/Images/UserInfoUnitTest.cs
@@ -1,25 +1,17 @@
 ﻿using Microsoft.VisualStudio.TestPlatform.UnitTestFramework;
-using Mntone.Nico2.Dictionaries.Summary;
+using Mntone.Nico2.Images.Users.Info;
 
-namespace Mntone.Nico2.Test.Dictionaries
+namespace Mntone.Nico2.Test.Images
 {
 	[TestClass]
-	public sealed class SummaryUnitTest
+	public sealed class UserInfoUnitTest
 	{
 		[TestMethod]
-		public void Summary_0通常データ()
+		public void UserInfo_0通常データ()
 		{
-			var ret = SummaryClient.ParseSummaryData( TestHelper.Load( @"Dictionaries/Summary/default.jsonp" ) );
-			Assert.AreEqual( "sm9", ret.Title );
-			Assert.AreEqual( "sm9", ret.ViewTitle );
-			Assert.AreEqual( "概要\n1000万再生達成\n 400万コメント達成\n β時代の英雄\n ニコニコ動画に現存するSMILEVIDEOの最古の動画で、ニコニコ動画を代表する動画。公式動画第一号である。レッツゴー！陰陽師、陰陽", ret.Summary );
-		}
-
-		[TestMethod]
-		public void Summary_1エラーデータ()
-		{
-			var ret = SummaryClient.ParseSummaryData( TestHelper.Load( @"Dictionaries/Summary/null.jsonp" ) );
-			Assert.IsNull( ret );
+			var ret = InfoClient.ParseInfoData( TestHelper.Load( @"Images/Users/Info/default.xml" ) );
+			Assert.AreEqual( 2u, ret.UserID );
+			Assert.AreEqual( "戀塚", ret.UserName );
 		}
 	}
 }
